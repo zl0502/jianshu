47bbd3e1e0e94011757f881b1cb3c6cb134b4999
zl0502 <1459816730@qq.com>
2019-03-07 10:12:53 +0800

    cmmit AOP model

--- IoC/src/main/java/com/spring/Boss.java
+++ IoC/src/main/java/com/spring/Boss.java
@@ -1,23 +1,18 @@
 package com.spring;
-
 import java.util.List;
-
 public class Boss {
     private String name;
     private String company;
     private Car car;
     private List<String> hobbys;
-
     public Boss() {
     }
-
     public Boss(String name, String company, Car car, List<String> hobbys) {
         this.name = name;
         this.company = company;
         this.car = car;
         this.hobbys = hobbys;
     }
-
     public String getName() {
         return name;
     }
@@ -37,19 +32,15 @@ public class Boss {
     public Car getCar() {
         return car;
     }
-
     public void setCar(Car car) {
         this.car = car;
     }
-
     public List<String> getHobbys() {
         return hobbys;
     }
-
     public void setHobbys(List<String> hobbys) {
         this.hobbys = hobbys;
     }
-
     @Override
     public String toString() {
         return "Boss{" +
